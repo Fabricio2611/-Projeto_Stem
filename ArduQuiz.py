c38c503f3fa3e034230707832c18de1c8eaa19b4
Fabricio2611 <83986929+Fabricio2611@users.noreply.github.com>
2021-05-11 11:06:34 -0400

    Add files via upload

---
+++ ArduQuiz.py
@@ -0,0 +1,58 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class Manager(ScreenManager):
+    pass
+
+
+class Menu(Screen):
+    pass
+
+
+class Game(Screen):
+    pass
+
+
+class Basico(Screen):
+    pass
+
+
+class Entrada(Screen):
+    pass
+
+
+class Saida(Screen):
+    pass
+
+
+class Controle(Screen):
+    pass
+
+
+class Matematica(Screen):
+    pass
+
+
+class Comentario(Screen):
+    pass
+
+
+class Variavel(Screen):
+    pass
+
+
+class Medio(Screen):
+    pass
+
+
+class Avancado(Screen):
+    pass
+
+
+class ArduQuiz(App):
+    pass
+
+
+if __name__ == "__main__":
+    ArduQuiz().run()
